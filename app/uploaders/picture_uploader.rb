02be1e090e29ca74a659ed19327ea0e0291f1f8d
garrettstott <garrettstott@gmail.com>
2016-05-06 16:22:28 -0600

    fixed typo

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,7 +7,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.evn.production?
+  if Rails.env.production?
     storage :fog
   else
     storage :file
